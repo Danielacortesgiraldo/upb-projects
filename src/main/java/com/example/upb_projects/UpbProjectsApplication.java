882348bfd16cf023cf431a64a36c1a70849de61c
Danielacortesgiraldo <danielacortesgiraldo.1@gmail.com>
2025-05-21 19:45:57 -0500

    Initial configuration

---
+++ src/main/java/com/example/upb_projects/UpbProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.example.upb_projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpbProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpbProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/upb_projects/UpbProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.upb_projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpbProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
